4fee73b77fe51619c07b4eaf6543dc4010142960
ikayomech <41094109+ikayomech@users.noreply.github.com>
2018-10-05 11:39:51 +0900

    update recruiment

--- bot.py
+++ bot.py
@@ -154,4 +154,65 @@ async def on_message(message):
 		if client.user != message.author:
 			await client.send_message(message.channel, reply)
 
+			"""メンバー募集.rect@数字"""
+	elif message.content.startswith(".rect"):
+		mcount = int(message.content[6])
+		text= "あと{}人 募集中\n"
+		revmsg = text.format(mcount)
+		#friend_list 押した人のList
+		frelist = []
+		msg = await client.send_message(message.channel, revmsg)
+
+		#投票の欄
+		await client.add_reaction(msg, '◀')
+		await client.add_reaction(msg, '⏫')
+
+		#リアクションをチェックする
+		while len(frelist) < int(message.content[6:len(message.content)]):
+			target_reaction = await client.wait_for_reaction(message=msg)
+			#発言したユーザが同一でない場合 真
+			if target_reaction.user != msg.author:
+				#==============================================================
+				#押された絵文字が既存のものの場合 >> 左　del
+				if target_reaction.reaction.emoji == '◀':
+					#==========================================================
+					#◀のリアクションに追加があったら反応 frelistにuser.nameがあった場合　真
+					if target_reaction.user.name in frelist:
+						frelist.remove(target_reaction.user.name)
+						mcount += 1
+						#リストから名前削除
+						#for mslist in frelist:
+						await client.edit_message(msg, text.format(mcount) +
+														'\n'.join(frelist))
+							#メッセージを書き換え
+							#print('del {}'.format(mglist))
+
+					else:
+						pass
+				#==============================================================
+				#押された絵文字が既存のものの場合　>> 右　add
+				elif target_reaction.reaction.emoji == '⏫':
+					if target_reaction.user.name in frelist:
+						pass
+
+					else:
+						frelist.append(target_reaction.user.name)
+						#リストに名前追加
+						mcount = mcount - 1
+						await client.edit_message(msg, text.format(mcount) +
+														'\n'.join(frelist))
+
+
+				elif target_reaction.reaction.emoji == '✖':
+						await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
+						#await CLIENT.unpin_message(msg)
+						break
+
+				await client.remove_reaction(msg, target_reaction.reaction.emoji, target_reaction.user)
+				#bot以外のユーザーがつけたリアクションを消す
+				#==============================================================
+		else:
+			await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
+
+
 client.run("NDY3MTgxMTU3ODcyNjk3MzQ0.Do9LpQ.GXRLzbKePmAbVYCo-4pnsl2irZY")
