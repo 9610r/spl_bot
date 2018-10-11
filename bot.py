71e68af81e7bc8094442da670e3fe82f69c2f9ed
ikayomech <41094109+ikayomech@users.noreply.github.com>
2018-10-12 01:44:58 +0900

    update bot

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ async def on_voice_state_update(before, after):
         nowtime = datetime.now().strftime("%m/%d-%H:%M")
         #print(getserver.voice_channel)
 
-        if(before.voice_channel is None):
+		if(before.voice_channel is None):
 			intext=nowtime + "　に "+ after.name + "　が　"+ after.voice_channel.name + " に参加しました。"
 			vcchannel = client.get_channel('499906318308474890')
 			await client.send_message(vcchannel, intext)
