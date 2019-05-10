27caf010d19b1a18df3d29d840d5c7100ad5d34b
toratiger96 <41094109+ikayomech@users.noreply.github.com>
2019-05-11 02:23:14 +0900

    Add files via upload

--- bot_re.py
+++ bot_re.py
@@ -1,18 +1,21 @@
 #-*- -*- -*- -*- -*- -*- -*- -*- coding:UTF-8 -*- -*- -*- -*- -*- -*- -*- -*- -
 import json
-import discord
-#v1.0
+import discord #v1.0
 import requests
 import re
 from random import randint,choice
 import datetime
 import os
+from discord.ext import commands
 #-*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*
 
-DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
+#DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
 #LOG_CHANNEL_ID = os.environ["LOG_CHANNEL_ID"]
 #LOOK_SERVER_ID = os.environ["LOOK_SERVER_ID"]
-RECT_CHANNEL_ID = os.environ["RECT_CHANNEL_ID"]
+#RECT_CHANNEL_ID = os.environ["RECT_CHANNEL_ID"]
+
+client = commands.Bot(command_prefix='.', description='bot')
+sai = range(1,101)
 
 def getStageInfo(index=0):
 	'''ステージ情報'''
@@ -52,34 +55,8 @@ def getStageInfo(index=0):
 def randBuki(buki_list1, users):
 	len_u = len(users)
 	return {i:choice(buki_list1) for i in users}
-#clientオブジェクトの生成
-client = discord.Client()
 with open('buki.csv', encoding='UTF-8') as f:
 	buki_list1 = f.readlines()
-
-def randBuki_class(buki_list2, users):
-	len_u = len(users)
-	return {i:choice(buki_list2) for i in users}
-#clientオブジェクトの生成
-client = discord.Client()
-with open('buki_class.csv', encoding='UTF-8') as f:
-	buki_list2 = f.readlines()
-
-def randBuki_sub(buki_list3, users):
-	len_u = len(users)
-	return {i:choice(buki_list3) for i in users}
-#clientオブジェクトの生成
-client = discord.Client()
-with open('subweapon.csv', encoding='UTF-8') as f:
-	buki_list3 = f.readlines()
-
-def randBuki_sp(buki_list4, users):
-	len_u = len(users)
-	return {i:choice(buki_list4) for i in users}
-#clientオブジェクトの生成
-client = discord.Client()
-with open('special.csv', encoding='UTF-8') as f:
-	buki_list4 = f.readlines()
 
 def rand_Stage():
 	with open('stage.csv', encoding='UTF-8') as f:
@@ -88,110 +65,114 @@ def rand_Stage():
 		return stage_list
 
 
+
 @client.event
 async def on_ready():
 	print('Logged in as')
 	print(client.user.name)
 	print(client.user.id)
-	await client.change_presence(game=discord.Game(name="Splatoon2"))
+	await client.change_presence(activity=discord.Game(name="Spl2Bot v.1.1"))
 	print('------')
+
+@client.command()
+async def echo(ctx, what):
+    await ctx.send(f'{what}とはなんですか')
+
+@client.command()
+async def dice(ctx, ss: int):
+    if ss == '':
+        saikoro_choice = choice(sai)
+        print(saikoro_choice)
+        await ctx.send(saikoro_choice)
+    else :
+        saikoro_choice = choice(sai[:ss])
+        print(saikoro_choice)
+        await ctx.send(saikoro_choice)
+
+@client.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
+@client.command()
+async def remove(ctx, a: int, b: int):
+    await ctx.send(a-b)
+
+@client.command()
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
+
+@client.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+@client.command()
+async def random(ctx):
+	voice_channel = discord.utils.get(ctx.guild.channels, id=ctx.author.voice.channel.id)
+	p_list = voice_channel.members
+	voice_users= [ p_list[i].display_name for i in range(len(p_list))]
+	rand_buki1 = randBuki(buki_list1,voice_users)
+	mbuki1 = ''
+	stage = "test"
+	for i in rand_buki1.keys():
+		mbuki1 =  mbuki1 + '{}: {}'.format(i,rand_buki1[i])
+	msg = discord.Embed(title=stage,description=mbuki1, colour=0xffffff)
+	#msg.set_thumbnail(url="https://pbs.twimg.com/profile_images/819765217957552132/1WftJJM1_400x400.jpg")
+	await ctx.send(embed=msg)
+
+@client.command()
+async def info(ctx):
+    embed = discord.Embed(title="Splatoon2 bot", description="This bot is Spl2Bot.", color=0xeee657)
+
+    # give info about you here
+    embed.add_field(name="Author", value="ikayomeCh")
+
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+client.remove_command('help')
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(title="<<<Splatoon2 bot>>>", description="commands list:", color=0xeeefff)
+
+    embed.add_field(name="$add X Y", value="Gives the addition of **X** and **Y**", inline=False)
+    embed.add_field(name="$multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
+    embed.add_field(name="$greet", value="Gives a nice greet message", inline=False)
+    embed.add_field(name="$cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
+    embed.add_field(name="$info", value="Gives a little info about the bot", inline=False)
+    embed.add_field(name="$help", value="Gives this message", inline=False)
+
+    await ctx.send(embed=embed)
+
+
+
 
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('test'):
-        await message.channel.send('Hello')
-	#"random_buki"とチャットに入力があった場合反応
-	'''ランダム武器'''
-	if message.content.startswith('.random'):
-		voice_channel = discord.utils.get(message.server.channels, id=message.author.voice.voice_channel.id)
-		p_list = voice_channel.voice_members
-		voice_users= [ p_list[i].display_name for i in range(len(p_list))]
-		rand_buki1 = randBuki(buki_list1,voice_users)
-		mbuki1 = ''
-		stage = "ステージ："rand_Stage()
-		for i in rand_buki1.keys():
-			mbuki1 =  mbuki1 + '{}: {}'.format(i,rand_buki1[i])
-		msg = discord.Embed(title=stage,description=mbuki1, colour=0xffffff)
-		#msg.set_thumbnail(url="https://pbs.twimg.com/profile_images/819765217957552132/1WftJJM1_400x400.jpg")
-		await message.channel.send(embed=msg)
-
-	elif message.content.startswith('.randclass'):
-		voice_channel = discord.utils.get(message.server.channels, id=message.author.voice.voice_channel.id)
-		p_list = voice_channel.voice_members
-		voice_users= [ p_list[i].display_name for i in range(len(p_list))]
-		rand_buki2 = randBuki_class(buki_list2,voice_users)
-		mbuki2 = ''
-		for i in rand_buki2.keys():
-			mbuki2 =  mbuki2 + '{}:{}'.format(i,rand_buki2[i])
-		msg = discord.Embed(title='ブキの種類を決めるよ',description=mbuki2, colour=0xffffff)
-		#msg.set_thumbnail(url="https://pbs.twimg.com/profile_images/819765217957552132/1WftJJM1_400x400.jpg")
-		await message.channel.send(embed=msg)
-
-	elif message.content.startswith('.randsub'):
-		voice_channel = discord.utils.get(message.server.channels, id=message.author.voice.voice_channel.id)
-		p_list = voice_channel.voice_members
-		voice_users= [ p_list[i].display_name for i in range(len(p_list))]
-		rand_buki3 = randBuki_sub(buki_list3,voice_users)
-		mbuki3 = ''
-		for i in rand_buki3.keys():
-			mbuki3 =  mbuki3 + '{}:{}'.format(i,rand_buki3[i])
-		msg = discord.Embed(title='サブを決めるよ',description=mbuki3, colour=0xffffff)
-		#msg.set_thumbnail(url="https://pbs.twimg.com/profile_images/819765217957552132/1WftJJM1_400x400.jpg")
-		await message.channel.send(embed=msg)
-
-	elif message.content.startswith('.randsp'):
-		voice_channel = discord.utils.get(message.server.channels, id=message.author.voice.voice_channel.id)
-		p_list = voice_channel.voice_members
-		voice_users= [ p_list[i].display_name for i in range(len(p_list))]
-		rand_buki4 = randBuki_sp(buki_list4,voice_users)
-		mbuki4 = ''
-		for i in rand_buki4.keys():
-			mbuki4 =  mbuki4 + '{}:{}'.format(i,rand_buki4[i])
-		msg = discord.Embed(title='スペシャルを決めるよ',description=mbuki4, colour=0xffffff)
-		#msg.set_thumbnail(url="https://pbs.twimg.com/profile_images/819765217957552132/1WftJJM1_400x400.jpg")
-		await message.channel.send(embed=msg)
-
-	elif message.content.startswith('.help'):
-		# 送り主がBotだった場合反応しない
-		if client.user != message.author:
-			m = "これを見てね！\n" +"https://qiita.com/IkayomeCh/items/d490fd996d37beee494f"
-			# メッセージが送られてきたチャンネルへメッセージ送信
-			await message.channel.send( m)
-
-	elif message.content.startswith("サイコロ"):
-		saikoro_choice = choice(['1','2','3','4','5','6'])
-		if client.user != message.author:
-			# メッセージを書きます
-			m = saikoro_choice
-			await message.channel.send(,'{} {}'.format(message.author.mention,m))
-
-	elif client.user.id in message.content:
-		await message.channel.send( '{} 呼んだか？'.format(message.author.mention))
-
-	elif message.content.startswith('.gachi'):
+	if message.content.startswith('.gachi'):
 		m = getStageInfo(0)
-		await message.channel.send( embed=m)
+		await message.channel.send(embed=m)
 
 	elif message.content.startswith("..gachi"):
 		m = getStageInfo(1)
-		await message.channel.send( embed=m)
+		await message.channel.send(embed=m)
 
 	elif message.content.startswith(".league"):
 		m = getStageInfo(2)
-		await message.channel.send( embed=m)
+		await message.channel.send(embed=m)
 
 	elif message.content.startswith("..league"):
 		m = getStageInfo(3)
-		await message.channel.send( embed=m)
+		await message.channel.send(embed=m)
 
 	elif message.content.startswith(".nawabari"):
 		m = getStageInfo(4)
-		await message.channel.send( embed=m)
+		await message.channel.send(embed=m)
 
 	elif message.content.startswith("..nawabari"):
 		m = getStageInfo(5)
-		await message.channel.send( embed=m)
+		await message.channel.send(embed=m)
 
 	elif message.content.startswith(".salmon"):
 		headers = {"User-Agent": "IKSbot/1.0(twitter @ikayomech)"}
@@ -211,13 +192,12 @@ async def on_message(message):
 			nowinfo = "≪開催中！!≫"
 		else:
 			nowinfo = "≪シフト予定≫"
-	    msg = discord.Embed(title='サーモンラン '+nowinfo, description=time,colour=0xFB7E00)
+
+		msg = discord.Embed(title='サーモンラン'+nowinfo, description=time,colour=0xFB7E00)
 		msg.set_thumbnail(url=dic['stage']['image'])
 		msg.add_field(name=dic['stage']['name'],value=dic['weapons'][0]['name']+'\n'+dic['weapons'][1]['name']+'\n'+dic['weapons'][2]['name']+'\n'+dic['weapons'][3]['name'], inline=True)
 		await message.channel.send(embed=msg)
-
-    await client.process_commands(message)
-    #Erorr回避
+	await client.process_commands(message)
 
 
 client.run(DISCORD_TOKEN)
